644b1ab3bbece825f9adb293eb6b29ce773b4ff9
matthewglennpickering <matthewglennpickering@gmail.com>
2022-12-12 02:23:10 -0800

    index.html

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,4 +1,7 @@
 const selectionButtons = document.querySelectorAll('[data-selection]')
+const finalColumn = document.querySelector('[data-final-column]')
+const computerScoreSpan = document.querySelector('[data-computer-score]')
+const yourScoreSpan = document.querySelector('[data-your-score]')
 const SELECTIONS = [
     {
         name: 'rock',
@@ -29,14 +32,25 @@ function makeSelection(selection) {
     const computerSelection = randomSelection()
     const yourWinner = isWinner(selection, computerSelection)
     const computerWinner = isWinner(computerSelection, selection)
-    console.log(computerSelection)
+
+    addSelectionResult(computerSelection, computerWinner)
+    addSelectionResult(selection, yourWinner)
+
+   if (yourWinner) incrementScore(yourScoreSpan)
+   if (computerWinner) incrementScore(computerScoreSpan)
 }
 
-addSelectionResult(computerSelection, computerWinner)
-addSelectionResult(selection, yourWinner)
+
+function incrementScore(scoreSpan) {
+    scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1
+}
 
 function addSelectionResult(selection, winner) {
-
+    const div = document.createElement('div')
+    div.innerText = selection.emoji
+    div.classList.add('result-selection')
+    if (winner) div.classList.add('winner')
+    finalColumn.after(div)
 }
 
 function isWinner(selection, opponentSelection) {
