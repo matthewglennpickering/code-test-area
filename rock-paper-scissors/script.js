e862bb11a7bdfa870e449e6e56ded91755f234be
matthewglennpickering <matthewglennpickering@gmail.com>
2022-12-12 01:40:42 -0800

    script.js

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,18 +1,43 @@
 const selectionButtons = document.querySelectorAll('[data-selection]')
 const SELECTIONS = [
     {
-        name: 'rock'
-        emoji: '✊'
+        name: 'rock',
+        emoji: '✊',
+        beats: 'scissors'
+    },
+    {
+        name: 'paper',
+        emoji: '✋',
+        beats: 'rock'
+    },
+    {
+        name: 'scissors',
+        emoji: '✌️',
+        beats: 'paper'
     }
 ]
 
 selectionButtons.forEach(selectionButton => {
     selectionButton.addEventListener('click', e => {
         const selectionName = selectionButton.dataset.selection
-        makeSelection(selectionName)
+        const selection = SELECTIONS.find(selection => selection.name === selectionName)
+        makeSelection(selection)
     })
 })
 
 function makeSelection(selection) {
-    console.log(selection)
+    const computerSelection = randomSelection()
+    const yourWinner = isWinner(selection, computerSelection)
+    const computerWinner = isWinner(computerSelection, selection)
+    console.log(computerSelection)
 }
+
+function isWinner(selection, opponentSelection) {
+    return selection.beats === opponentSelection.name 
+}
+
+function randomSelection() {
+    const randomIndex = Math.floor(Math.random() * SELECTIONS.length)
+    return SELECTIONS[randomIndex]
+}
+
